d027e8b640898c38143a3f290336b7bd83d22fce
Malik Baktiyarov <malik@undev.ru>
2012-09-19 22:01:56 +0400

    Problem#22

---
+++ problem22.erl
@@ -0,0 +1,28 @@
+-module(problem22).
+
+-export([start/0]).
+
+-define(ALPHA, lists:seq($A, $Z)).
+
+score_str(Str) ->
+  lists:foldl(fun(Chr, Sum) -> Sum + string:str(?ALPHA, [Chr]) end, 0, Str).
+
+score(S) ->
+  {Scores, _} = lists:mapfoldl(fun(Str, I) ->
+  	{score_str(Str) * I, I + 1}
+  end, 1, S),
+
+  lists:foldl(fun(Score, Sum) -> Sum + Score end, 0, Scores).
+
+start() ->
+  {ok, Binary} = file:read_file("22.txt"),
+  
+  L = lists:map(fun(Term) ->
+  	S = erlang:length(Term) - 2,
+  	<<"\"", String:S/binary, "\"">> = erlang:list_to_binary(Term),
+  	binary:bin_to_list(String)
+  end, string:tokens(binary:bin_to_list(Binary), ",")),
+
+  Sum = score(lists:sort(L)),
+  
+  io:format("~p~n", [Sum]).
