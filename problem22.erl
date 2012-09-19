5bb0f49e9f4e057e4e5bc34ff942dbfcc8024255
Malik Baktiyarov <malikbakt@gmail.com>
2012-09-20 00:23:10 +0400

    Update problem22.erl

--- problem22.erl
+++ problem22.erl
@@ -9,7 +9,7 @@ score_str(Str) ->
 
 score(S) ->
   {Scores, _} = lists:mapfoldl(fun(Str, I) ->
-  	{score_str(Str) * I, I + 1}
+    {score_str(Str) * I, I + 1}
   end, 1, S),
 
   lists:foldl(fun(Score, Sum) -> Sum + Score end, 0, Scores).
@@ -18,9 +18,9 @@ start() ->
   {ok, Binary} = file:read_file("22.txt"),
   
   L = lists:map(fun(Term) ->
-  	S = erlang:length(Term) - 2,
-  	<<"\"", String:S/binary, "\"">> = erlang:list_to_binary(Term),
-  	binary:bin_to_list(String)
+    S = erlang:length(Term) - 2,
+    <<"\"", String:S/binary, "\"">> = erlang:list_to_binary(Term),
+    binary:bin_to_list(String)
   end, string:tokens(binary:bin_to_list(Binary), ",")),
 
   Sum = score(lists:sort(L)),
